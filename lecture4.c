12469af8e557936aa763b59fda9928289eb22ec3
ParulARMY <87768125+ParulARMY@users.noreply.github.com>
2021-12-10 11:48:29 +0530

    Add files via upload

---
+++ lecture4.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int two=2,three=3,six=6;
+int result = (two+three) * six/three;
+printf("I am new to programming but I know how to write arithmetic expression like this:(%d + %d) * %d/%d ",two,three,six,three);
+printf("and the result of this expression is :%d",result);
+return 0;
+}
